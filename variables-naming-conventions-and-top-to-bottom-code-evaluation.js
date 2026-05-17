11ca1a92ab6490b37eeb4e2b90231eac754aea49
garriethcrockett@gmail.com <garriethcrockett@gmail.com>
2026-05-16 21:26:56 -0500

    revised variables

--- variables-naming-conventions-and-top-to-bottom-code-evaluation.js
+++ variables-naming-conventions-and-top-to-bottom-code-evaluation.js
@@ -24,9 +24,9 @@ Things to reflect on:
   
 */
 
-let a = "Alice";
-let b = 5;
-let c = 20;
-let d = a + " bought " + b + " items for $" + c + ".";
+let customerName = "Alice";
+let numberOfItems = 5;
+let totalPrice = 20;
+let message = customerName + " bought " + numberOfItems + " items for $" + totalPrice + ".";
 
-console.log(d);
+console.log(message);
